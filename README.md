4760a845b02df90237322eafdf8fcf4a97a09a64
Pedro Lucas <pedrolucaspro@outlook.com>
2025-02-24 10:49:34 +0000

    commited LAB_10

---
+++ README.md
@@ -0,0 +1,2 @@
+# LAB_10
+## Algoritimo que mostra como se aplica as variáveis em Python.
---
+++ Variaveis.py
@@ -0,0 +1,22 @@
+'''
+   Programador...: (C) 2025 Pedro Sousa
+   Data..........: 12/02/2025
+   Observações...:Estudo e análise sobre variáveis
+'''
+
+numero = 23          # Variavel do tipo inteiro
+nota = 19.56         # Variável de tipo real
+nome = "Ana Jorge"   # Variável do tipo string
+resposta = True      # Variável do tipo booleano
+
+print("Tipos das variáveis")
+print("Numero ->", type(numero))
+print("nota", type (nota))
+print("nome", type (nome))
+print("resposta", type (resposta))
+
+resultado = numero + 56 #Operação soma
+
+print(f"O resultado entre {numero} + 56 = {resultado}")
+
+print(nome + "Silva") #Concatenação 
